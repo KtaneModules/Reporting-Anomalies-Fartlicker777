7dc21c850413424541449e04f8071383e0cd3295
Apolo Simon <62307787+Fartlicker777@users.noreply.github.com>
2023-03-07 02:51:12 -0600

    Bedroom done

---
+++ Reporting Anomalies/Assets/BedroomAnomalies.cs
@@ -0,0 +1,415 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+public class BedroomAnomalies : MonoBehaviour {
+
+   public AudioSource Music;
+
+   public ReportingAnomalies Mod;
+
+   public GameObject Intruder;
+   public GameObject[] ExtraObjects;
+   int ExtraObj;
+
+   public GameObject[] ObjectDisappearance;
+   int DisObj;
+   bool PCDis;
+   bool BedDis;
+
+   public GameObject[] LightAnomaly;
+   public GameObject Door;
+
+   public GameObject[] ObjectMovement;
+   bool PCMove;
+   int MovedObject;
+   bool BedMove;
+   //public GameObject[] FakeMovements;
+   //public Vector3[] StartingMovementVals = new Vector3[3];
+
+   public GameObject[] PosterAnomaly;
+   int TypeOfPainting;
+
+   public GameObject Abyss;
+   //0.1095824, 0.002272631, 0.1095824
+   //23.41538, 0.002272631, 23.41538
+
+   Coroutine IntruderCor;
+   Coroutine MusicFazCor;
+   Coroutine AbyssCor;
+
+   public bool[] ActiveAnomalies = new bool[9];
+
+   public void CheckFix () {
+      Mod.ActiveAnomalies--;
+      switch (Mod.AnomalyType) {
+         case 0:
+            FixIntruder();
+            break;
+         case 1:
+            FixExtra();
+            break;
+         case 2:
+            FixDisappear();
+            break;
+         case 3:
+            FixLight();
+            break;
+         case 4:
+            FixDoor();
+            break;
+         case 5:
+            FixCamera();
+            break;
+         case 6:
+            FixMove();
+            break;
+         case 7:
+            FixPainting();
+            break;
+         case 8:
+            FixAbyss();
+            break;
+      }
+   }
+
+   public void ChooseAnomaly () {
+      Mod.ActiveAnomalies++;
+      int RandomAnomaly = Rnd.Range(0, 9);
+      do {
+         RandomAnomaly = Rnd.Range(0, 9);
+      } while (ActiveAnomalies[RandomAnomaly]);
+
+      ActiveAnomalies[RandomAnomaly] = true;
+
+      switch (RandomAnomaly) {
+         case 0:
+            IntruderInit();
+            break;
+         case 1:
+            ExtraInit();
+            break;
+         case 2:
+            DisappearInit();
+            break;
+         case 3:
+            LightInit();
+            break;
+         case 4:
+            DoorInit();
+            break;
+         case 5:
+            CameraInit();
+            break;
+         case 6:
+            MoveInit();
+            break;
+         case 7:
+            PaintingInit();
+            break;
+         case 8:
+            AbyssInit();
+            break;
+      }
+   }
+
+   #region Intruder
+
+   public void IntruderInit () {
+      Intruder.SetActive(true);
+      IntruderCor = StartCoroutine(Fabore());
+      Music.Play();
+   }
+
+   public void FixIntruder () {
+      Intruder.SetActive(false);
+      Music.Stop();
+      StopCoroutine(IntruderCor);
+   }
+
+   IEnumerator Fabore () {
+      //Intruder.SetActive(true);
+      while (true) {
+         var duration = .25f;
+         var elapsed = 0f;
+         while (elapsed < duration) {
+            Intruder.transform.localEulerAngles = new Vector3(270, Mathf.Lerp(0, 359, elapsed / duration), 0);
+            yield return null;
+            elapsed += Time.deltaTime;
+         }
+      }
+   }
+
+   #endregion
+
+   #region Extra Object
+
+   public void ExtraInit () {
+      ExtraObj = Rnd.Range(0, ExtraObjects.Length);
+      ExtraObjects[ExtraObj].SetActive(true);
+   }
+
+   public void FixExtra () {
+      ExtraObjects[ExtraObj].SetActive(false);
+      ExtraObj = -1;
+   }
+
+   #endregion
+
+   #region Disappearing object
+
+   public void DisappearInit () {
+      do {
+         DisObj = Rnd.Range(0, ObjectDisappearance.Length);
+      } while (PCMove && DisObj > 4);
+      ObjectDisappearance[DisObj].SetActive(false);
+      if (DisObj > 4) {
+         PCDis = true;
+      }
+      else {
+         BedDis = true;
+      }
+   }
+
+   public void FixDisappear () {
+      ObjectDisappearance[DisObj].SetActive(true);
+      DisObj = -1;
+      PCDis = false;
+      BedDis = false;
+   }
+
+   #endregion
+
+   #region Light Anomaly
+
+   public void LightInit () {
+      LightAnomaly[0].SetActive(false);
+      LightAnomaly[1].SetActive(false);
+   }
+
+   public void FixLight () {
+      LightAnomaly[0].SetActive(true);
+      LightAnomaly[1].SetActive(true);
+   }
+
+   #endregion
+
+   #region Door Opening
+
+   public void DoorInit () {
+      //Debug.Log(Door);
+      StartCoroutine(MoveDoor());
+   }
+
+   public void FixDoor () {
+      //Debug.Log(Door);
+      StartCoroutine(ResetDoor());
+   }
+
+   IEnumerator MoveDoor () {
+      //Debug.Log("Pemus");
+      var duration = .25f;
+      var elapsed = 0f;
+      while (elapsed < duration) {
+         Door.transform.localEulerAngles = new Vector3(270, Mathf.Lerp(90, 130, elapsed / duration), 0);
+         yield return null;
+         elapsed += Time.deltaTime;
+      }
+   }
+
+   IEnumerator ResetDoor () {
+      var duration = .25f;
+      var elapsed = 0f;
+      while (elapsed < duration) {
+         Door.transform.localEulerAngles = new Vector3(270, Mathf.Lerp(130, 90, elapsed / duration), 0);
+         yield return null;
+         elapsed += Time.deltaTime;
+      }
+   }
+
+   #endregion
+
+   #region Painting
+
+   public void PaintingInit () {
+      //PosterAnomaly[0].SetActive(false);
+      TypeOfPainting = Rnd.Range(0, 3);
+      //TypeOfPainting = 0;
+      Debug.Log(TypeOfPainting);
+      if (TypeOfPainting == 0) {
+         PosterAnomaly[0].GetComponent<SpriteRenderer>().flipX = false;
+      }
+      else {
+         PosterAnomaly[0].SetActive(false);
+         PosterAnomaly[TypeOfPainting].SetActive(true);
+      }
+   }
+
+   public void FixPainting () {
+      PosterAnomaly[0].GetComponent<SpriteRenderer>().flipX = true;
+      if (TypeOfPainting != 0) {
+         PosterAnomaly[0].SetActive(true);
+         PosterAnomaly[TypeOfPainting].SetActive(false);
+      }
+      TypeOfPainting = -1;
+   }
+
+   #endregion
+
+   #region Object Movement
+
+   public void MoveInit () {
+      do {
+         MovedObject = Rnd.Range(0, 3);
+      } while ((PCDis && MovedObject == 1) || (PCMove && MovedObject == 2));
+      
+      if (MovedObject == 1) {
+         PCMove = true;
+      }
+      else if (MovedObject == 2) {
+         BedMove = true;
+      }
+
+      StartCoroutine(ShowMove());
+      //ObjectMovement[MovedObject].SetActive(false);
+      //FakeMovements[MovedObject].SetActive(true);
+   }
+
+   public IEnumerator ShowMove () {
+      var duration = .25f;
+      var elapsed = 0f;
+      switch (MovedObject) {
+         case 0:
+
+            while (elapsed < duration) {
+               ObjectMovement[0].transform.localEulerAngles = new Vector3(0, Mathf.Lerp(47.814f, 90f, elapsed / duration), 0);
+               yield return null;
+               elapsed += Time.deltaTime;
+            }
+            
+            break;
+         case 1:
+            while (elapsed < duration) {
+               ObjectMovement[1].transform.localPosition = new Vector3(7.2052f, 1.405f, Mathf.Lerp(-5.2043f, -4.5f, elapsed / duration));
+               yield return null;
+               elapsed += Time.deltaTime;
+            }
+            break;
+         case 2:
+            while (elapsed < duration) {
+               ObjectMovement[2].transform.localPosition = new Vector3(Mathf.Lerp(-.39f, 0.17f, elapsed / duration), 0.04f, Mathf.Lerp(-6.853f, -6.45f, elapsed / duration));
+               ObjectMovement[2].transform.localEulerAngles = new Vector3(0, Mathf.Lerp(0, -21.163f, elapsed / duration), 0);
+               yield return null;
+               elapsed += Time.deltaTime;
+            }
+            break;
+         default:
+            
+            yield return null;
+            break;
+      }
+   }
+
+   public IEnumerator FixMoveAnim () {
+      var duration = .25f;
+      var elapsed = 0f;
+      switch (MovedObject) {
+         case 0:
+
+            while (elapsed < duration) {
+               ObjectMovement[0].transform.localEulerAngles = new Vector3(0, Mathf.Lerp(90f, 47.814f, elapsed / duration), 0);
+               yield return null;
+               elapsed += Time.deltaTime;
+            }
+
+
+
+            break;
+         case 1:
+            while (elapsed < duration) {
+               ObjectMovement[1].transform.localPosition = new Vector3(7.2052f, 1.405f, Mathf.Lerp(-4.5f, -5.2043f, elapsed / duration));
+               yield return null;
+               elapsed += Time.deltaTime;
+            }
+            break;
+         case 2:
+            while (elapsed < duration) {
+               ObjectMovement[2].transform.localPosition = new Vector3(Mathf.Lerp(0.17f, -.39f, elapsed / duration), 0.04f, Mathf.Lerp(-6.45f, -6.853f, elapsed / duration));
+               ObjectMovement[2].transform.localEulerAngles = new Vector3(0, Mathf.Lerp(-21.163f, 0, elapsed / duration), 0);
+               yield return null;
+               elapsed += Time.deltaTime;
+            }
+            break;
+         default:
+
+            yield return null;
+            break;
+      }
+   }
+
+   public void FixMove () {
+      StartCoroutine(FixMoveAnim());
+      MovedObject = -1;
+      BedMove = false;
+      PCMove = false;
+   }
+
+   #endregion
+
+   #region Camera Malfunction
+
+   public void CameraInit () {
+      Mod.BrokenCam = 0;
+      if (Mod.CameraPos == 0) {
+         Mod.RightButton.OnInteract();
+      }
+   }
+
+   public void FixCamera () {
+      Mod.BrokenCam = -1;
+   }
+
+   #endregion
+
+   #region Abyss
+
+   public void AbyssInit () {
+      AbyssCor = StartCoroutine(AbyssGrow(Abyss.transform.localScale));
+   }
+
+   public void FixAbyss () {
+      if (AbyssCor != null) {
+         StopCoroutine(AbyssCor);
+      }
+      AbyssCor = StartCoroutine(AbyssGrow(Abyss.transform.localScale));
+   }
+
+   public IEnumerator AbyssGrow (Vector3 From) {
+      Debug.Log(From);
+      var duration = 20f;
+      var elapsed = 0f;
+      Vector3 To = new Vector3(23.41538f, 0.002272631f, 23.41538f);
+      while (Abyss.transform.localScale.x < 23.41538f) {
+         Abyss.transform.localScale = Vector3.Lerp(From, To, elapsed / duration);
+         //Debug.Log(Abyss.transform.localScale);
+         yield return null;
+         elapsed += Time.deltaTime;
+      }
+   }
+
+   public IEnumerator AbyssShrink (Vector3 From) {
+      Debug.Log(From);
+      var duration = 20f;
+      var elapsed = 0f;
+      Vector3 To = new Vector3(0.1095824f, 0.002272631f, 0.1095824f);
+      while (Abyss.transform.localScale.x < 23.41538f) {
+         Abyss.transform.localScale = Vector3.Lerp(From, To, elapsed / duration);
+         //Debug.Log(Abyss.transform.localScale);
+         yield return null;
+         elapsed += Time.deltaTime;
+      }
+   }
+
+   #endregion
+}
---
+++ Reporting Anomalies/Assets/ReportingAnomalies.cs
@@ -0,0 +1,262 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KModkit;
+using Rnd = UnityEngine.Random;
+using UnityEngine.UI;
+
+
+public class ReportingAnomalies : MonoBehaviour {
+
+   public KMBombInfo Bomb;
+   public KMAudio Audio;
+
+   public KMSelectable LeftButton;
+   public KMSelectable ReportButton;
+   public KMSelectable RightButton;
+
+   public GameObject WarningSystem;
+   public Text WarningT;
+
+   public GameObject PleaseStandBy;
+
+   public KMSelectable[] AnomalousButtons;
+   public KMSelectable[] RoomSelectionButtons;
+   public KMSelectable BackButton;
+   public KMSelectable SendButton;
+   public GameObject Menu;
+   public Material UnselectedButton;
+   public Material SelectedButton;
+   //public GameObject WrongCanvas;
+   public Text WrongText;
+
+   public Text ReportPending;
+
+   public Material[] CameraMats;
+   public GameObject Screen;
+   public TextMesh NowViewingText;
+   string[] RoomNames = { "bedroom", "dungeon"};
+   public BedroomAnomalies Yeah;
+   public int BrokenCam = -1;
+
+   public int RoomLocation = -1;
+   public int AnomalyType = -1;
+
+   public int ActiveAnomalies;
+
+   string[] IntroText = {
+      "ATTENTION EMPLOYEE",
+      "ANOMALIES HAVE BEEN PREVIOUSLY SIGHTED IN THE MONITORED HOUSE",
+      "PAY EXTREMELY CLOSE ATTENTION TO THE SURVEILLANCE CAMERA FOOTAGE",
+      "FILE AN ANOMALY REPORT ASAP WHEN YOU NOTICE SOMETHING HAS CHANGED"
+   };
+
+   bool Filing;
+
+   string WrongText1 = "No anomaly of type ";
+   string[] AnomalyTypesStr = { "intruder", "extra object", "object disappearance", "light anomaly", "door opening", "camera malfunction", "object movement", "painting anomaly", "abyss presence"};
+   string WrongText2 = " found in ";
+   //string[] LocationNamesStr = { "bedroom", "dungeon", "fur den"};
+
+   public int CameraPos;
+
+   static int ModuleIdCounter = 1;
+   int ModuleId;
+   private bool ModuleSolved;
+
+   void Awake () {
+      ModuleId = ModuleIdCounter++;
+      
+      foreach (KMSelectable B in AnomalousButtons) {
+          B.OnInteract += delegate () { AnomButtPress(B); return false; };
+      }
+
+      foreach (KMSelectable B in RoomSelectionButtons) {
+         B.OnInteract += delegate () { RoomButtPress(B); return false; };
+      }
+
+
+      LeftButton.OnInteract += delegate () { LeftPress(); return false; };
+      RightButton.OnInteract += delegate () { RightPress(); return false; };
+
+      ReportButton.OnInteract += delegate () { OpenMenu(); return false; };
+      BackButton.OnInteract += delegate () { CloseMenu(); return false; };
+      SendButton.OnInteract += delegate () { FileReport(); return false; };
+
+   }
+
+   void AnomButtPress (KMSelectable B) {
+      for (int i = 0; i < AnomalousButtons.Length; i++) {
+         if (B == AnomalousButtons[i]) {
+            for (int j = 0; j < 9; j++) {
+               AnomalousButtons[j].GetComponent<MeshRenderer>().material = UnselectedButton;
+            }
+            AnomalousButtons[i].GetComponent<MeshRenderer>().material = SelectedButton;
+            AnomalyType = i;
+         }
+      }
+   }
+
+   void RoomButtPress (KMSelectable B) {
+      for (int i = 0; i < RoomSelectionButtons.Length; i++) {
+         if (B == RoomSelectionButtons[i]) {
+            for (int j = 0; j < 3; j++) {
+               RoomSelectionButtons[j].GetComponent<MeshRenderer>().material = UnselectedButton;
+            }
+            RoomSelectionButtons[i].GetComponent<MeshRenderer>().material = SelectedButton;
+            RoomLocation = i;
+         }
+      }
+   }
+
+   void Start () {
+      Menu.SetActive(false);
+      StartCoroutine(StartAnim());
+      StartCoroutine(Test());
+   }
+
+   void OpenMenu () {
+      Audio.PlaySoundAtTransform("Tick", ReportButton.transform);
+      if (Filing) {
+         return;
+      }
+      Menu.SetActive(true);
+   }
+
+   void CloseMenu () {
+      Audio.PlaySoundAtTransform("Tick", BackButton.transform);
+      Menu.SetActive(false);
+   }
+
+   void FileReport () {
+      //Audio.PlaySoundAtTransform("Tick", SendButton.transform);
+      if (RoomLocation == -1 || AnomalyType == -1) {
+         return;
+      }
+      Filing = true;
+      for (int i = 0; i < 9; i++) {
+         AnomalousButtons[i].GetComponent<MeshRenderer>().material = UnselectedButton;
+         RoomSelectionButtons[i / 3].GetComponent<MeshRenderer>().material = UnselectedButton;
+      }
+      CloseMenu();
+      StartCoroutine(WaitForAnomalousReport());
+      //Debug.Log(Yeah.ActiveAnomalies[AnomalyType]);
+      
+   }
+
+   IEnumerator WaitForAnomalousReport () {
+      for (int j = 0; j < 2; j++) {
+         ReportPending.text = "Report pending";
+         for (int i = 0; i < 3; i++) {
+            yield return new WaitForSeconds(1f);
+            ReportPending.text += ".";
+         }
+         yield return new WaitForSeconds(1f);
+      }
+      ReportPending.text = "";
+      switch (RoomLocation) {
+         case 0:
+            if (Yeah.ActiveAnomalies[AnomalyType]) {
+               StartCoroutine(FixingScreen());
+            }
+            else {
+               StartCoroutine(WrongAnomaly());
+            }
+            break;
+         default:
+            break;
+      }
+   }
+
+   IEnumerator FixingScreen () {
+      PleaseStandBy.SetActive(true);
+      Audio.PlaySoundAtTransform("Fixing", transform);
+      yield return new WaitForSeconds(1.368f);
+      PleaseStandBy.SetActive(false);
+      Yeah.CheckFix();
+   }
+
+   IEnumerator WrongAnomaly () {
+      Filing = false;
+      //WrongCanvas.SetActive(true);
+      WrongText.text = WrongText1 + AnomalyTypesStr[AnomalyType] + WrongText2 + RoomNames[RoomLocation] + ".";
+      WrongText.color = new Color(WrongText.color.r, WrongText.color.g, WrongText.color.b, 0);
+      var duration = .25f;
+      while (WrongText.color.a < 1.0f) {
+         WrongText.color = new Color(WrongText.color.r, WrongText.color.g, WrongText.color.b, WrongText.color.a + (Time.deltaTime / duration));
+         yield return null;
+      }
+      yield return new WaitForSeconds(5f);
+      while (WrongText.color.a != 0) {
+         WrongText.color = new Color(WrongText.color.r, WrongText.color.g, WrongText.color.b, WrongText.color.a - (Time.deltaTime / duration));
+         yield return null;
+      }
+      Reset();
+   }
+
+   void Reset () {
+      AnomalyType = -1;
+      RoomLocation = -1;
+   }
+
+   IEnumerator StartAnim () {
+      yield return new WaitForSeconds(3f);
+      WarningSystem.SetActive(true);
+      Audio.PlaySoundAtTransform("Warning Noise", transform);
+      yield return new WaitForSeconds(2f);
+      for (int j = 0; j < 4; j++) {
+         for (int i = 0; i < IntroText[j].Length; i++) {
+            WarningT.text += IntroText[j][i].ToString();
+            yield return new WaitForSeconds(.08f);
+         }
+         yield return new WaitForSeconds(1f);
+         WarningT.text = "";
+      }
+      WarningSystem.SetActive(false);
+   }
+
+   IEnumerator Test () {
+      yield return new WaitForSeconds(2f);
+      Yeah.ChooseAnomaly();
+      //Yeah.MoveInit();
+   }
+
+   void LeftPress () {
+      Audio.PlaySoundAtTransform("Tick", LeftButton.transform);
+      CameraPos--;
+      CameraPos = CameraPos < 0 ? CameraPos + CameraMats.Length : CameraPos;
+      if (BrokenCam == CameraPos) {
+         CameraPos--;
+         CameraPos = CameraPos < 0 ? CameraPos + CameraMats.Length : CameraPos;
+      }
+      Screen.GetComponent<MeshRenderer>().material = CameraMats[CameraPos];
+      NowViewingText.text = "Now viewing: " + RoomNames[CameraPos];
+   }
+
+   void RightPress () {
+      Audio.PlaySoundAtTransform("Tick", RightButton.transform);
+      CameraPos++;
+      CameraPos %= CameraMats.Length;
+      if (BrokenCam == CameraPos) {
+         CameraPos++;
+         CameraPos %= CameraMats.Length;
+      }
+      Screen.GetComponent<MeshRenderer>().material = CameraMats[CameraPos];
+      NowViewingText.text = "Now viewing: " + RoomNames[CameraPos];
+   }
+
+#pragma warning disable 414
+   private readonly string TwitchHelpMessage = @"Use !{0} to do something.";
+#pragma warning restore 414
+
+   IEnumerator ProcessTwitchCommand (string Command) {
+      yield return null;
+   }
+
+   IEnumerator TwitchHandleForcedSolve () {
+      yield return null;
+   }
+}
